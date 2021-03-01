63ee921996ac54c7745ddc0fd727fe6b22542217
Andreas Hansen <andreashansen513@outlook.com>
2021-03-01 14:07:15 +0100

    Car Spawner

---
+++ GD6 Game 3/Assets/Enemy Script/CarSpawner.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+
+    public float SpawnDelay = .3f;
+
+    float NextTimeToSpawn = 0f;
+
+    public GameObject car;
+
+    public Transform[] SpawnPoints;
+  
+    void Update()
+    {
+
+        if (NextTimeToSpawn >= Time.time)
+        {
+            SpawnCar();
+            NextTimeToSpawn = Time.time + SpawnDelay;
+        }
+
+
+
+
+    }
+
+
+    void SpawnCar()
+    {
+
+        int RandomIndex = Random.Range (0, SpawnPoints.Length);
+        Transform SpawnPoint = SpawnPoints[RandomIndex];
+
+
+        Instantiate(car, SpawnPoint.position, SpawnPoint.rotation);
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
